96ab9085590c6921b7e6ba84fd66489e56170a19
Rosilie Lim <rosilielim@gmail.com>
2023-08-04 13:21:54 -0500

    updated pdf url path

--- notes/urls.py
+++ notes/urls.py
@@ -5,7 +5,7 @@ from .views import NotesListView
 urlpatterns = [
     path('',views.index,name='notes'),
     path('notes-list/',NotesListView.as_view(),name='note-list-view'),
-    path('pdf/<int:note_id>',views.note_render_pdf_views,name='note-pdf-view'),
+    path('pdf/<int:note_id>/',views.note_render_pdf_views,name='note-pdf-view'),
     path('<int:note_id>',views.note,name='note'),
     path('search',views.search,name='search'),
 ]
